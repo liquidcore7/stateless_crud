78f7b857c17dd0b5cb80c4ab8a9bcec8ca0bb80b
Dan Y <liquidcore7@gmail.com>
2019-02-12 12:34:01 +0200

    Force comparison by id
    Mark hashCode() and equals() final to prevent overriding

--- app/models/CrudEntity.scala
+++ app/models/CrudEntity.scala
@@ -4,9 +4,9 @@ package models
 abstract class CrudEntity[T](val id: Long, val compressed: String) {
   val body: T
 
-  override def hashCode(): Int = id.hashCode
+  final override def hashCode(): Int = id.hashCode
 
-  override def equals(o: Any): Boolean = o match {
+  final override def equals(o: Any): Boolean = o match {
     case sameType: CrudEntity[T] => sameType.id == this.id
     case _ => false
   }
