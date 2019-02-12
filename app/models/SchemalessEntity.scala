674c50b549757d69cd053b899ed6c0f2281641dd
Dan Y <liquidcore7@gmail.com>
2019-02-12 12:35:08 +0200

    Add fromId constructor

--- app/models/SchemalessEntity.scala
+++ app/models/SchemalessEntity.scala
@@ -1,5 +1,15 @@
 package models
 
-case class SchemalessEntity(override val id: Long, override val compressed: String) extends CrudEntity[String](id, compressed) {
+case class SchemalessEntity(override val id: Long, override val compressed: String)
+  extends CrudEntity[String](id, compressed) {
+
   override val body: String = compressed
+}
+
+object SchemalessEntity {
+  private val defaultBody: String = ""
+
+  // as CrudEntities are checked for equality based on id,
+  // use this method to create a index
+  def fromId(id: Long): SchemalessEntity = SchemalessEntity(id, defaultBody)
 }
