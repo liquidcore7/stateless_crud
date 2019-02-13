eef3432a8c8880aec2c47b3bd810c39bc0ad03d8
Dan Y <liquidcore7@gmail.com>
2019-02-13 18:47:35 +0200

    Fix gzip read/write
    Close gzip iostreams properly before querying result

--- app/helpers/GzipConverter.scala
+++ app/helpers/GzipConverter.scala
@@ -6,35 +6,46 @@ import java.util.zip.{GZIPInputStream, GZIPOutputStream}
 
 import com.google.common.base.Charsets
 
-import scala.io.Codec
-import scala.language.reflectiveCalls
+import scala.io.{Codec, Source}
 
-object GzipConverter extends BidirectionalConverter[String, String] {
+
+case class GzippedData(byteArray: Array[Byte])
+
+
+object GzipConverter extends BidirectionalConverter[String, GzippedData] {
 
   private val utf8: Charset = Charsets.UTF_8
 
-  override def direct(s: String): String = {
-    type Streams = (ByteArrayOutputStream, GZIPOutputStream)
+  override def direct(s: String): GzippedData = {
 
     val writeStreams = { byteStream: ByteArrayOutputStream =>
       val gzipStream = new GZIPOutputStream(byteStream)
       gzipStream.write(s.getBytes(utf8))
+      gzipStream.close()
 
-      byteStream -> gzipStream -> byteStream.toByteArray
+      byteStream -> byteStream.toByteArray
     }
 
-    writeStreams.andThen { case (streams: Streams, compressed: Array[Byte]) =>
-      closeAll(streams._1, streams._2)
-      new String(compressed, utf8)
+    writeStreams.andThen { case (stream, byteArray) =>
+      stream.close()
+      GzippedData(byteArray)
     }.apply(new ByteArrayOutputStream(s.length))
   }
 
-  private def closeAll(closeables: {def close(): Unit}*): Unit = {
-    closeables.foreach(_.close())
+  override def inverse(a: GzippedData): String = {
+
+    val readStreams = { inputStream: ByteArrayInputStream =>
+      val gzipStream = new GZIPInputStream(inputStream)
+      val result: String = Source.fromInputStream(gzipStream)(Codec.UTF8).mkString
+      gzipStream.close()
+
+      inputStream -> result
+    }
+
+    readStreams.andThen { case (stream, result) =>
+        stream.close()
+        result
+    }.apply(new ByteArrayInputStream(a.byteArray))
   }
 
-  override def inverse(a: String): String = {
-    val inputStream = new GZIPInputStream(new ByteArrayInputStream(a.getBytes(utf8)))
-    scala.io.Source.fromInputStream(inputStream)(Codec.UTF8).mkString
-  }
 }
